b2f43d6ad562bf4543a6791e9c967159b38ba29c
C836 <gl.sepiol@gmail.com>
2022-05-18 01:32:00 -0300

    novos planetas, refatoração de código e arquivos

--- src/galaxy/Galaxy.tsx
+++ src/galaxy/Galaxy.tsx
@@ -1,24 +1,56 @@
-import './Sun.sass'
+import useDraggable from './../utils/useDraggable'
+import useWindowDimensions from '../utils/windowDimensions'
+import { useRef } from 'react'
+
 import './Galaxy.sass'
-import Mercury from '../components/planets/mercury/Mercury'
-import Venus from '../components/planets/venus/Venus'
+import './Sun.sass'
+import './../components/planets.sass'
+
+const DraggableSpace = ({ children }:any) => {
+    const cardRef = useRef(null);
+    useDraggable(cardRef);
+  
+    return (
+      <div className='draggableSpace' ref={cardRef}>
+        {children}
+      </div>
+    );
+};
 
 function Canva(){
+    const planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter"]
+    //, "Uranus", "Neptune"
+
     return(
         <section className='galaxy'>
-            <div className='planets_wrapper'>
-                <div id="sun" />
-                    
-                <Mercury />
-                <Venus />
+            <DraggableSpace>
+                <div className='planets_wrapper'>
+                    <div id="sun" />
 
-                {/* <figure>
-                    <img src={Earth.selected.picture} />
-                </figure>
-                <article>
-                    <h1>{Earth.selected.title}</h1>
-                </article> */}
-            </div>
+                    {planets.map((item, index)=>(
+                        <section id={item.toLowerCase()} className="orbit">
+                            <div className='pos'>
+                                <div className="planet"/>
+                            </div>
+                        </section>
+                    ))}
+                    
+                    <section id='saturn' className="orbit">
+                        <div className='pos'>
+                            <div className="planet">
+                                <div className="saturnRing"/>
+                            </div>
+                        </div>
+                    </section>
+
+                    {/* <figure>
+                        <img src={Earth.selected.picture} />
+                    </figure>
+                    <article>
+                        <h1>{Earth.selected.title}</h1>
+                    </article> */}
+                </div>
+            </DraggableSpace>
         </section>
     )
 }
